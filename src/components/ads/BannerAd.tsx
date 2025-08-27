e50b9c344ca70b6b31f67daca5ccabfbf6ccb02a
Kflix <darayoeurn@gmail.com>
2025-08-27 06:29:32 -0400

    Update BannerAd.tsx

--- src/components/ads/BannerAd.tsx
+++ src/components/ads/BannerAd.tsx
@@ -9,12 +9,12 @@ interface BannerAdProps {
   id: string;
 }
 
-const BannerAd: React.FC<BannerAdProps> = ({ adKey, width = 728, height = 90, id }) => {
+const BannerAd: React.FC<BannerAdProps> = ({ adKey, width = 300, height = 250, id }) => {
   useEffect(() => {
     // Load Adsterra script dynamically
     const script = document.createElement("script");
     script.async = true;
-    script.src = `https://pl${adKey}.adsterra.com/${id}/invoke.js`;
+    script.src = `https://www.highperformanceformat.com/297ed86b29d181f660479ebab2f97717/invoke.js`;
     document.getElementById(id)?.appendChild(script);
   }, [adKey, id]);
 
